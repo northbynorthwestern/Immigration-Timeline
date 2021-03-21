265470312aff9933f9706f9216970ad692e56542
Kevin Rueda <65484407+RuedaKV@users.noreply.github.com>
2021-03-20 20:49:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let article1 = "insert"
+let article2 = "insert"
+let article3 = "insert"
+let article4 = "insert"
+let article5 = "insert"
+let article6 = "insert"
+
+document.querySelector(".card1").onclick = function(){window.open(article1)}
+
+document.querySelector(".card2").onclick = function(){window.open(article2)}
+
+document.querySelector(".card3").onclick = function(){window.open(article3)}
+
+document.querySelector(".card4").onclick = function(){window.open(article4)}
+
+document.querySelector(".card5").onclick = function(){window.open(article5)}
+
+document.querySelector(".card6").onclick = function(){window.open(article6)}
